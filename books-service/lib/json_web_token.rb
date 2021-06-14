7e0fe4a4ed6e014f8a84e42a155bd3c497776f01
Vlada Mamutova <vlada.mamutova@gmail.com>
2021-06-14 13:07:53 +0300

    chore(books): add path to master key in kube deployment

--- books-service/lib/json_web_token.rb
+++ books-service/lib/json_web_token.rb
@@ -2,8 +2,8 @@
 class JsonWebToken
   class << self
     # Rails.application.secrets.secret_key_base can be null in production
-    # so use Rails.application.secret_key_base 
-    SECRET_KEY = Rails.application.secret_key_base 
+    # so use Rails.application.secret_key_base
+    SECRET_KEY = Rails.application.secret_key_base
     EXPIRATION_TIME_INTERVAL = 30.minutes
 
     # JWT token = base64(header) + '.' + base64(payload) + '.' + base64(signature)
